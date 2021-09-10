a2bd7bae126e5fe7cb3850d2eefd069d2415a18b
Denys A Khomenko <deniskhomenko@yahoo.com>
2021-09-10 16:07:42 -0400

    version 4

--- src/action.js
+++ src/action.js
@@ -1,28 +1,39 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, until, wait} = require('selenium-webdriver');
 const chromedriver = require('chromedriver');
 const DriverPage = require("../src/driver")
-
+const axios = require("axios");
 
 
 class ActionPage extends DriverPage {
+    async findByXpath(xpath) {
+        await driver.findElement(By.xpath(xpath)).click()
+    }
+    async findByXpath1(xpath) {
+        await driver.findElements(By.xpath(xpath))
+    }
 
-     async landPage(url) {
-         await axios.get(url).then(response => {
-            return (response.status);
-        });
+    async timer(url){
+        let startTime = new Date().getTime();
+        await axios.get(url);
+        let endTime = new Date().getTime();
+        return (endTime - startTime);
+    }
+    async landPage(url) {
+        const response = await axios.get(url)
+        return response.status;
 
     }
 
-    async findLang(xpath) {
+    async findCurrentUrl(xpath) {
         await driver.findElement(By.xpath(xpath)).sendKeys(Key.RETURN)
         return driver.getCurrentUrl()
 
     }
 
-    getLangConformation(x) {
-        axios.get(x).then(response => {
+    async getLangConformation(x) {
+        const response = await axios.get(x)
             return response.data.slice(27, 32)
-        })
+
     }
 }
 
--- test/test1.js
+++ test/test1.js
@@ -1,6 +1,7 @@
 const action = require("../src/action")
-const {By, Key, until} = require("selenium-webdriver");
-
+const {By, Key, until, wait} = require("selenium-webdriver");
+const assert = require("assert");
+const axios = require("axios");
 
 
 describe('Game Flash For You Test Suite', function () {
@@ -9,46 +10,56 @@ describe('Game Flash For You Test Suite', function () {
     let germanPage = '//header/div[1]/div[1]/ul[1]/li[3]/a[1]';
     let ukrainianPage = '//header/div[1]/div[1]/ul[1]/li[2]/a[1]';
     let russianPage = '//header/div[1]/div[1]/ul[1]/li[1]/a[1]';
+    let top100Eng = '//header/div[1]/ul[1]/li[6]/ul[1]/li[1]/a[1]';
+    let otherButton = "//header/div[1]/ul[1]/li[6]/a[1]";
+    let list100 = "//*[@class='in']"
+    before("Before Tests", async () => {
+        await driver.get(baseurl)
+    })
+    after('Quit Test', async () => {
+        await driver.quit()
+    })
 
-    it.only('Server response 200 OK', async () => {
-        let expectedValue = action.landPage(baseurl);
-        let expectedValue2 = await axios.get(baseurl).then(response => {
-            return (response.status);
-        });
+    it('Server response 200 OK', async () => {
+        let expectedValue = await action.landPage(baseurl);
 
-        await expect(expectedValue2).to.be.equal(200, 'Response status has to be 200');
+        await expect(expectedValue).to.be.equal(200, 'Response status has to be 200');
 
     })
 
     it('Response Time of Server is below 3100', async () => {
-        let startTime = new Date().getTime();
-        // let endTime = await axios.get(baseurl).then(response => {
-        //     return new Date().getTime();
-        // });
-        await axios.get(baseurl);
-        let endTime = new Date().getTime();
-        let actualResult = (endTime - startTime);
+        let actualResult = await action.timer(baseurl);
         await expect(actualResult).to.be.below(3400, 'This is server load time')
     })
     it('Linkage test, switch to German page', async () => {
-        global.expectedUrl = await action.findLang(germanPage);
+        global.expectedUrl = await action.findCurrentUrl(germanPage);
         expect(expectedUrl).to.be.equal('https://de.gf4y.com/', 'This is URL of German page')
 
     })
     it('Test of page switching to German language', async () => {
-        //let url = await action.findLang(germanPage);
-        let expectedLanguage = await axios.get(expectedUrl).then(response => {
-            return response.data.slice(27, 32)
-        });
+        let expectedLanguage = await action.getLangConformation(expectedUrl)
         await expect(expectedLanguage).to.be.equal('de-de', 'Language is German')
 
     })
-    it('Top 100 games displays more than 100 results', function () {
+
+    it('Title of the page says ', async () => {
+        // title of new window can be used for assertions
+        let title = await driver.getTitle();
+        expect(title).to.be.equal('Online Spiele - Kostenlos Online spielen auf GF4Y.COM');
 
     })
-    it('Quit', function () {
-        driver.quit()
+    it('Return to English page', async () => {
+        let engUrl = await action.findCurrentUrl(germanPage);
+        expect(engUrl).to.be.equal('https://en.gf4y.com/', 'This is URL of English page');
     })
+    it('Top 100 games displays more than 100 results', async () => {
+        await action.findByXpath(otherButton);
+        await action.findByXpath(top100Eng);
+        let top = await driver.findElements(By.className('in'))
+        let actualResult = top.length
+        expect(actualResult).to.be.above(100)
+    })
+
 })
 
 
